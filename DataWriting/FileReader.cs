b850c0cdb69f1df5bc9651b0bb6cd60f29b9113a
Vladimír Antoš <vladimirantos@seznam.cz>
2015-04-14 23:01:01 +0200

    Dokončena třída pro načítání ze souboru

--- DataWriting/FileReader.cs
+++ DataWriting/FileReader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
+using System.IO;
 
 namespace RDB_Project.DataWriting
 {
@@ -14,14 +15,36 @@ namespace RDB_Project.DataWriting
 
     class FileReader : IFileReader
     {
+        string _path;
+
         public FileReader(string path)
         {
-
+            _path = path;
         }
 
         public void Read(BlockingCollection<string> output)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (FileStream fs = File.Open(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (BufferedStream bs = new BufferedStream(fs))
+                using (StreamReader sr = new StreamReader(bs))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        output.Add(line);
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                throw new FileNotFoundException(e.Message, e);
+            }
+            finally
+            {
+                output.CompleteAdding();
+            }
         }
     }
 }
